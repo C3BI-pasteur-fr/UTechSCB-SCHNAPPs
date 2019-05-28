5cdcad5356cc4534eb354a8dadfe947d6b6e7c6d
Bernd  JAGLA <bernd.jagla@pasteur.fr>
2019-05-28 16:36:29 +0200

    prep for master

--- inst/develo/renameVar.R
+++
@@ -1,6 +0,0 @@
-oldname = "renderPlotly"
-newName = "plotly::renderPlotly"
-system(paste0("find . -name \"*.R\"  -exec sed -i '' -e 's/", oldname, "/", newName,"/g' {} +"))
-
-
-system(paste0("find . -name \"*.Rmd\"  -exec sed -i '' -e 's/", oldname, "/", newName,"/g' {} +"))
