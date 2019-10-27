2a5ad8be3fb2eb8d270564b71f4cd7be771bb358
baj12 <bernd.jagla@pasteur.fr>
2019-10-27 15:49:58 +0100

    when genes are removed from scEx_log

--- inst/app/reactives.R
+++ inst/app/reactives.R
@@ -1406,7 +1406,7 @@ scExLogMatrixDisplay <- reactive({
   }
   retVal <-
     data.frame(
-      symbol = make.names(rowData(scEx)$symbol, unique = TRUE),
+      symbol = make.names(rowData(scEx)$symbol[rownames(scEx_log)], unique = TRUE),
       stringsAsFactors = FALSE
     )
   if (!is.null(scEx_log)) {
@@ -2715,12 +2715,15 @@ reacativeReport <- function() {
 
 consolidateScEx <-
   function(scEx, projections, scEx_log, pca, tsne) {
+    commCells = base::intersect(colnames(scEx), colnames(scEx_log))
+    commGenes = base::intersect(rownames(scEx), rownames(scEx_log))
+    scEx = scEx[commGenes, commCells]
     reducedDims(scEx) <- SimpleList(PCA = pca$x, TSNE = tsne)
-    assays(scEx)[["logcounts"]] <- assays(scEx_log)[[1]]
-    colData(scEx)[["before.Filter"]] <- projections$before.filter
-    colData(scEx)[["dbCluster"]] <- projections$dbCluster
-    colData(scEx)[["UmiCountPerGenes"]] <- projections$UmiCountPerGenes
-    colData(scEx)[["UmiCountPerGenes2"]] <- projections$UmiCountPerGenes2
+    assays(scEx)[["logcounts"]] <- assays(scEx_log)[[1]][commGenes, commCells]
+    colData(scEx)[["before.Filter"]] <- projections$before.filter[commGenes]
+    colData(scEx)[["dbCluster"]] <- projections$dbCluster[commGenes]
+    colData(scEx)[["UmiCountPerGenes"]] <- projections$UmiCountPerGenes[commGenes]
+    colData(scEx)[["UmiCountPerGenes2"]] <- projections$UmiCountPerGenes2[commGenes]
     
     return(scEx)
   }
