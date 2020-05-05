9c6362f4b6bc893a6ef31d3fdcf0ddbc7a128940
Bernd <bernd@BerndsMacPro.local>
2020-05-05 12:33:45 +0200

    lite-version 4.12 save to history not working

--- inst/app/reactives-lite.R
+++ inst/app/reactives-lite.R
@@ -46,7 +46,11 @@ projections <- reactive({
   # data. Here we ensure that everything is loaded and all varialbles are set by waiting
   # input data being loaded
   scEx <- scEx()
-  pca <- pca()
+  # pca is already fixed in the calculation so we don't need to recalculate it.
+  pca <- NULL
+  
+  # in schnapps-lite we are only interested in adding the sessionProjections
+  # to the already calculated other projections
   prjs <- sessionProjections$prjs
   newPrjs <- projectionsTable$newProjections
   if (.schnappsEnv$DEBUGSAVE) {
@@ -66,6 +70,7 @@ projections <- reactive({
   # load(file="~/SCHNAPPsDebug/projections.RData"); DEBUGSAVE=FALSE
   
   
+  # browser()
   
   # todo colData() now returns a s4 object of class DataFrame
   # not sure what else is effected...
@@ -76,79 +81,76 @@ projections <- reactive({
   }
   projections <- pd
   
-  if (!is.null(pca)) {
-    comColNames = colnames(projections) %in% colnames(pca$x)
-    colnames(projections)[comColNames] = paste0(colnames(projections)[comColNames], ".old")
-    projections <- cbind(projections, pca$x[rownames(projections),])
-  }
-  
-  withProgress(message = "Performing projections", value = 0, {
-    n <- length(.schnappsEnv$projectionFunctions)
-    iter <- 1
-    for (proj in .schnappsEnv$projectionFunctions) {
-      start.time1 <- Sys.time()
-      incProgress(1 / n, detail = paste("Creating ", proj[1]))
-      if (DEBUG) {
-        cat(file = stderr(), paste("calculation projection:  ", proj[1], "\n"))
-      }
-      if (DEBUG) cat(file = stderr(), paste("projection: ", proj[2], "\n"))
-      assign("tmp", eval(parse(text = paste0(proj[2], "()"))))
-      if (.schnappsEnv$DEBUGSAVE) {
-        save(
-          file = paste0("~/SCHNAPPsDebug/projections.", iter, ".RData"),
-          list = c("tmp")
-        )
-        iter <- iter + 1
-      }
-      # load(file="~/SCHNAPPsDebug/projections.1.RData")
-      # browser()
-      # TODO here, dbCluster is probably overwritten and appended a ".1"
-      if (is(tmp, "data.frame")) {
-        cn <- make.names(c(colnames(projections), colnames(tmp)), unique = TRUE)
-      } else {
-        cn <- make.names(c(colnames(projections), make.names(proj[1])), unique = TRUE)
-      }
-      if (length(tmp) == 0) {
-        next()
-      }
-      if (ncol(projections) == 0) {
-        # never happening because we set pca first
-        projections <- data.frame(tmp = tmp)
-      } else {
-        if (nrow(projections) == length(tmp)) {
-          projections <- cbind(projections, tmp)
-        } else {
-          if (!is.null(nrow(tmp))) {
-            if (nrow(projections) == nrow(tmp)) {
-              projections <- cbind(projections, tmp)
-            }
-          } else {
-            save(file = "~/SCHNAPPsDebug/projectionsError.RData", list = c(ls()))
-            stop("error: ", proj[1], "didn't produce a result, please send file ~/SCHNAPPsDebug/projectionsError.RData to bernd")
-          }
-        }
-        # else {
-        #   stop("error: ", proj[1], "didn't produce a result")
-        # }
-      }
-      if (!length(colnames(projections)) == length(cn)) {
-        save(file = "~/SCHNAPPsDebug/projectionsError2.RData", list = c(ls()))
-        stop("error: ", proj[1], "didn't produce a result, please send file ~/SCHNAPPsDebug/projectionsError2.RData to bernd")
-      }
-      colnames(projections) <- cn
-      if (DEBUG) cat(file = stderr(), paste("colnames ", paste0(colnames(projections), collapse = " "), "\n"))
-      if (DEBUG) cat(file = stderr(), paste("observe this: ", proj[2], "\n"))
-      # observe(proj[2], quoted = TRUE)
-    }
-  })
+  # Commenting out during dev. here we are usually calculating the user defined new projections like tsne, umap
+  # etc which are already calculated.
+  # 
+  # withProgress(message = "Performing projections", value = 0, {
+  #   n <- length(.schnappsEnv$projectionFunctions)
+  #   iter <- 1
+  #   for (proj in .schnappsEnv$projectionFunctions) {
+  #     start.time1 <- Sys.time()
+  #     incProgress(1 / n, detail = paste("Creating ", proj[1]))
+  #     if (DEBUG) {
+  #       cat(file = stderr(), paste("calculation projection:  ", proj[1], "\n"))
+  #     }
+  #     if (DEBUG) cat(file = stderr(), paste("projection: ", proj[2], "\n"))
+  #     assign("tmp", eval(parse(text = paste0(proj[2], "()"))))
+  #     if (.schnappsEnv$DEBUGSAVE) {
+  #       save(
+  #         file = paste0("~/SCHNAPPsDebug/projections.", iter, ".RData"),
+  #         list = c("tmp")
+  #       )
+  #       iter <- iter + 1
+  #     }
+  #     # load(file="~/SCHNAPPsDebug/projections.1.RData")
+  #     # browser()
+  #     # TODO here, dbCluster is probably overwritten and appended a ".1"
+  #     if (is(tmp, "data.frame")) {
+  #       cn <- make.names(c(colnames(projections), colnames(tmp)), unique = TRUE)
+  #     } else {
+  #       cn <- make.names(c(colnames(projections), make.names(proj[1])), unique = TRUE)
+  #     }
+  #     if (length(tmp) == 0) {
+  #       next()
+  #     }
+  #     if (ncol(projections) == 0) {
+  #       # never happening because we set pca first
+  #       projections <- data.frame(tmp = tmp)
+  #     } else {
+  #       if (nrow(projections) == length(tmp)) {
+  #         projections <- cbind(projections, tmp)
+  #       } else {
+  #         if (!is.null(nrow(tmp))) {
+  #           if (nrow(projections) == nrow(tmp)) {
+  #             projections <- cbind(projections, tmp)
+  #           }
+  #         } else {
+  #           save(file = "~/SCHNAPPsDebug/projectionsError.RData", list = c(ls()))
+  #           stop("error: ", proj[1], "didn't produce a result, please send file ~/SCHNAPPsDebug/projectionsError.RData to bernd")
+  #         }
+  #       }
+  #       # else {
+  #       #   stop("error: ", proj[1], "didn't produce a result")
+  #       # }
+  #     }
+  #     if (!length(colnames(projections)) == length(cn)) {
+  #       save(file = "~/SCHNAPPsDebug/projectionsError2.RData", list = c(ls()))
+  #       stop("error: ", proj[1], "didn't produce a result, please send file ~/SCHNAPPsDebug/projectionsError2.RData to bernd")
+  #     }
+  #     colnames(projections) <- cn
+  #     if (DEBUG) cat(file = stderr(), paste("colnames ", paste0(colnames(projections), collapse = " "), "\n"))
+  #     if (DEBUG) cat(file = stderr(), paste("observe this: ", proj[2], "\n"))
+  #     # observe(proj[2], quoted = TRUE)
+  #   }
+  # })
   # add a column for gene specific information that will be filled/updated on demand
   # projections$UmiCountPerGenes <- 0
   # projections$UmiCountPerGenes2 <- 0
-  for (pdIdx in colnames(pd)) {
-    if (!pdIdx %in% colnames(projections)) {
-      projections[, pdIdx] <- pd[, pdIdx]
-    }
-  }
+  # for (pdIdx in colnames(pd)) {
+  #   if (!pdIdx %in% colnames(projections)) {
+  #     projections[, pdIdx] <- pd[, pdIdx]
+  #   }
+  # }
   
   if (ncol(prjs) > 0 & nrow(prjs) == nrow(projections)) {
     projections <- cbind(projections, prjs)
@@ -172,10 +174,10 @@ projections <- reactive({
   if (!"sampleNames" %in% colnames(projections)) {
     projections$sampleNames <- "1"
   }
-  add2history(type = "save", input = input, comment = "projections", projections = projections)
+  # TODO figure out how to limit this.
+  # add2history(type = "save", input = input, comment = "projections", projections = projections)
   
-  # add2history(type = "save", comment = "projections", projections = projections)
-  
+
   exportTestValues(projections = {
     projections
   })
