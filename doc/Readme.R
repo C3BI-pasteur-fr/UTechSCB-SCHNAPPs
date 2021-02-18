74e26c187ca2d13e4409a841120e49f97ddf6f19
Bernd Jagla <bernd.jagla@pasteur.fr>
2021-02-18 08:30:26 +0100

    doc

---
+++ doc/Readme.R
@@ -0,0 +1,9 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, warning=FALSE, message=FALSE, error=FALSE-------------------------
+library(SCHNAPPs)
+
